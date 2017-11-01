f59e609bcb8785ed3540b33adb532e6f8647fb97
hm.song <hm.song5149@gmail.com>
2017-11-02 01:35:17 +0900

    불필요한 설정 제거 & JPA 연동
    feature : 불필요한 설정 제거 & JPA 연동

--- src/main/java/hm/song/blog/config/SecurityWebApplicationInitializer.java
+++
@@ -1,6 +0,0 @@
-package hm.song.blog.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
-}
