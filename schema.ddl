8cd393a8034182091e2a3b9dbb258d584b5e512b
Unknown <hm.song5149@gmail.com>
2018-03-23 22:45:14 +0900

    스키마 파일 추가 및 코드 정리
    스키마 파일 추가 및 코드 정리

--- schema.ddl
+++ schema.ddl
@@ -1,3 +1,3 @@
-create table POST (ID integer not null auto_increment, IS_DISPLAY bit not null, MOD_DATE datetime not null, REG_DATE datetime not null, TITLE varchar(255) not null, CONTENTS longtext not null, primary key (ID))
-create table TAG (POST_ID integer not null, TAG varchar(64) not null, primary key (POST_ID, TAG))
-alter table TAG add constraint FKgd4sna4a522j8q4fjr8vcc368 foreign key (POST_ID) references POST (ID)
+create table POST (ID integer not null auto_increment, IS_DISPLAY bit not null, MOD_DATE datetime not null, REG_DATE datetime not null, TITLE varchar(255) not null, CONTENTS longtext not null, primary key (ID));
+create table TAG (POST_ID integer not null, TAG varchar(64) not null, primary key (POST_ID, TAG));
+alter table TAG add constraint FKgd4sna4a522j8q4fjr8vcc368 foreign key (POST_ID) references POST (ID);
--- src/main/java/hm/song/blog/web/AdminPostController.java
+++ src/main/java/hm/song/blog/web/AdminPostController.java
@@ -26,28 +26,11 @@ public class AdminPostController {
 		return service.writePost(title, display, contents, tags);
 	}
 
-	@GetMapping(value = "/posts/{id}/modify/view")
-	public String getModifyView(Principal principal, @PathVariable int id, Model model) {
-		Post post = service.getPost(id, principal != null);
-		model.addAttribute("id", post.getId());
-		model.addAttribute("title", post.getTitle());
-		model.addAttribute("contents", post.getContents());
-		model.addAttribute("regDate", post.getReadableRegDate());
-		return "modify";
-	}
-
 	@PostMapping(value = "/posts/{id}/modify")
 	public int updatePost(@PathVariable int id, String title,
 	                      boolean display, String contents, String[] tags) {
 		logger.info("updatePost() - id={}, title={}, display={}, contentsSize={}, tags={}",
 				id, title, display, contents.length(), tags);
 		return service.updatePost(id, title, contents, display, tags);
-	}
-
-	@PostMapping(value = "/posts/{id}/remove")
-	@ResponseBody
-	public void removePost(@PathVariable int id) {
-		logger.info("hidePost - id={}", id);
-		service.hidePost(id);
 	}
 }
